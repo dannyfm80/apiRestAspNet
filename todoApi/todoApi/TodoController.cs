78fbac557cba86286b2e14adc0f1b53892c99b75
Sergio Danny Flores M <dannyfm80@gmail.com>
2018-05-10 21:43:55 -0400

    first restAsp

---
+++ todoApi/todoApi/TodoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using todoApi.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace todoApi.Controllers
+{
+    public class TodoController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
